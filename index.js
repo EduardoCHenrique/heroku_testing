9acb367db977a40a6236b1899f5345c1260a9bd4
Du Henrique <eduardochenrique@gmail.com>
2016-12-08 14:04:11 -0200

    wip

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// var app = require('express')();
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+//
+// app.get('/', function(req, res) {
+//   console.log('requisição de teste');
+//   res.send('deu certo ae mano')
+// });
+//
+// app.listen(process.env.PORT || 3332, function() {
+//   console.log('Rodando na porta 3332');
+// })
 
-app.get('/', function(req, res) {
-  console.log('requisição de teste');
-  res.send('deu certo ae mano')
+
+
+var express = require("express");
+var mysql      = require('mysql');
+var app = express();
+app.use(express.logger());
+
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-04.cleardb.net',
+  user     : 'be9d41e962e89d',
+  password : 'eeb022b7',
+  database : 'heroku_ab7c55c476b592d'
 });
 
-app.listen(process.env.PORT || 3332, function() {
-  console.log('Rodando na porta 3332');
-})
+connection.connect();
+
+app.get('/', function(request, response) {
+  connection.query('SELECT * from t_users', function(err, rows, fields) {
+      if (err) {
+        console.log('error: ', err);
+        throw err;
+      }
+      response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
